bb17d7498fd62a66dc4a2e61ca2291385aea3bfd
EniV90 <eni_v90@yahoo.com>
2023-02-28 14:36:38 +0100

    code update

--- script.js
+++ script.js
@@ -1,45 +1,80 @@
-function validateForm() {
-  // Get the form values
-  const fname = document.forms["registrationForm"]["fname"].value;
-  const lname = document.forms["registrationForm"]["lname"].value;
-  const tel = document.forms["registrationForm"]["tel"].value;
-  const email = document.forms["registrationForm"]["email"].value;
-  const password = document.forms["registrationForm"]["password"].value;
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstName");
+const lastname = document.getElementById("lastName");
+const phonenumber = document.getElementById("phoneNumber");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
 
-    const form = document.getElementById("registrationForm");
-    let errorMessage = document.getElementsById("errorMessage");
-    errorMessage.textContent = "Please enter a valid email address";
-    errorMessage.style.color = "red";
-    errorMessage.style.fontSize = "12px";
-    form.appendChild(errorMessage);
-
-  // Check that all required fields are filled out
-  if (
-    fname == "" ||
-    lname == "" ||
-    tel == "" ||
-    email == "" ||
-    password == ""
-  ) {
-    // alert("This field is empty");
-    return errorMessage;
-  }
-
-  // Check that the email is properly formatted
-  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-    alert("Please enter a valid email address.");
-    return false;
-  }
-
-  // Check that the password and confirm password fields match
-  if (!/^ (2=. * [A-Za-z]) (?=.*\d) [A-Za-z\d] {8, ]$/g) {
-    alert(
-      "Password should be atleast 8 characters, one capital and small letter"
-    );
-    return false;
-  }
-
-  // If all validation checks pass, submit the form
-  return true;
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
+
+function showSucces(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSucces(input);
+  } else {
+    showError(input, "Email is not invalid");
+  }
+}
+
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSucces(input);
+    }
+  });
+}
+
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be les than ${max} characters`
+    );
+  } else {
+    showSucces(input);
+  }
+}
+
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkPassword(input1) {
+  if (input1 == "") {
+    showError(input1, "Password field empty");
+  }
+}
+
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkRequired([firstName, lastName, phoneNumber, email, password]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPassword(password);
+});
